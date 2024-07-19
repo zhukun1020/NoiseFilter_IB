aa9b55156d7e4d14edd672f088a50fa824799356
zhukun1020 <137504144+zhukun1020@users.noreply.github.com>
2024-07-19 11:46:19 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,131 @@ import os
 import random
 
 
+def clean_output(text: str) -> str:
+    """Extract out the answer text portion.
+
+    prediction: e.g., "The above context is (-23.0) helpful. answer: Violet Alva"
+    returns: e.g., "Violent Alva"
+    """
+    text = text.replace("Answer", "answer")
+    if "answer:" in text:
+        text = text.split("answer:")[-1].strip()
+
+    text = text.replace("Response", "response")
+    if "response:" in text:
+        text = text.split("response:")[-1].strip()
+
+    text = text.replace("Judgement", "judgement")
+    if "judgement:" in text:
+        text = text.split("judgement:")[-1].strip()
+
+    text = text.replace("Score", "score")
+    if "score:" in text:
+        text = text.split("score:")[-1].strip()
+    if "answer:" in text:
+        text = text.split("answer:")[0].strip()
+
+    return text
+
+
+def normalize_text(text: str) -> str:
+    """Normalize text with lowercasing, removing articles, and punctuation."""
+
+    def remove_articles(text: str) -> str:
+        return re.sub(r"\b(a|an|the)\b", " ", text)
+
+    def white_space_fix(text: str) -> str:
+        return " ".join(text.split())
+
+    def remove_punc(text: str) -> str:
+        exclude = set(string.punctuation)
+        exclude.add("'")
+        exclude.add('"')
+        return "".join(ch for ch in text if ch not in exclude)
+
+    def lower(text: str) -> str:
+        return text.lower()
+
+    return white_space_fix(remove_punc(remove_articles(lower(text))))
+
+
+def has_answer(text: str, answers: list[str]) -> float:
+    """Check if text contains any of the answers."""
+    norm_text = normalize_text(text)
+    norm_answers = [normalize_text(ans) for ans in answers]
+    # print(text,norm_text)
+    return float(any([(ans in norm_text) for ans in norm_answers]))
+    # return float(any([(ans.lower() in text.lower()) for ans in answers]))
+
+
+def calc_exact_match(text: str, answers: list[str]) -> bool:
+    """Check if prediction is exactly the same as any of the answers."""
+    norm_text = normalize_text(text)
+    norm_answers = [normalize_text(ans) for ans in answers]
+    # print(norm_text,norm_answers)
+    return max([(norm_text == norm_ans) for norm_ans in norm_answers])
+
+
+def calc_unigram_f1(text: str, answers: list[str], field: str = "f1") -> float:
+    """Calculate unigram f1 score between the text and reference answers."""
+    norm_pred = normalize_text(text)
+    norm_answers = [normalize_text(ans) for ans in answers]
+    common_tokens = [
+        Counter(norm_pred) & Counter(norm_ans) for norm_ans in norm_answers
+    ]
+    num_same = [sum(common.values()) for common in common_tokens]
+
+    score_list = []
+    for i, num in enumerate(num_same):
+        if num == 0:
+            score_list.append(0.0)
+        else:
+            p = 1.0 * num / len(norm_pred)
+            r = 1.0 * num / len(norm_answers[i])
+            f1 = 2 * p * r / (p + r)
+            if field == "precision":
+                score_list.append(p)
+            elif field == "recall":
+                score_list.append(r)
+            elif field == "f1":
+                score_list.append(f1)
+            else:
+                raise ValueError(f"Unknown field: {field}")
+    return max(score_list)
+
+
+def extract_pred(answer_ori):
+    # return answer_ori
+    # answer_ori = lower(answer_ori)
+    # print(answer_ori)
+
+    if "a summary of the contexts related to the question: " in answer_ori:
+        return answer_ori.split("a summary of the contexts related to the question: ")[-1].strip()
+    if "Excerpt:" in answer_ori:
+        # print(answer_ori.split("Contribution:")[0].split("Excerpt:")[-1].strip())
+        return answer_ori.split("Contribution:")[0].split("Excerpt:")[-1].strip()
+
+    if "[/INST]" in answer_ori:
+        answer_ori = answer_ori.split("[/INST]")[-1].strip()
+    if "Answer:" in answer_ori:
+        answer_ori = answer_ori.split("Answer:")[-1].strip()
+
+    pattern = r"\[(.+)\]"
+    # print(answer_ori)
+    res = re.findall(pattern, answer_ori)
+    # if len(res) > 1:
+    #     if res[0] != res[1]:
+    #         print(answer_ori)
+    #         print("____")
+    # print(answer_ori,res)
+    # print("-----")
+    if len(res) > 0:
+        return res[0]
+
+    # return answer_ori
+    return answer_ori.strip().split("\n")[0].strip()
+
+
 def pre_anwer_prompt_nortv(question):
     prompt = '''<s>[INST] <<SYS>>
     You are a helpful, respectful and honest assistant. 
@@ -437,8 +562,6 @@ def _get_word_ngrams(n, sentences):
     words = sum(sentences, [])
     # words = [w for w in words if w not in stopwords]
     return _get_ngrams(n, words)
-
-
 
 
 if __name__=="__main__":
